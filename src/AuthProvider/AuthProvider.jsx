a4262b41e2d1af7ed17c7c661ec9095ad113d04b
Md Saiful Islam Rafel <mdsaifulisalmrafel@gmail.com>
2024-04-07 04:03:03 +0600

    private router added

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,20 +1,23 @@
 /* eslint-disable react/prop-types */
-import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     const GoogleProvider = new GoogleAuthProvider();
     const FacebookProvider = new FacebookAuthProvider();
     const GithubProvider = new GithubAuthProvider();
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password)
     };
 
@@ -23,25 +26,33 @@ const AuthProvider = ({ children }) => {
     };
 
     const googleSignIn = () => {
+        setLoading(true);
         return signInWithPopup(auth, GoogleProvider);
     };
     const facebookSignIn = () => {
+        setLoading(true);
         return signInWithPopup(auth, FacebookProvider);
     };
     const githubSignIn = () => {
+        setLoading(true);
         return signInWithPopup(auth, GithubProvider);
+    };
+
+    const resetPassword = (email) => {
+        return sendPasswordResetEmail(auth, email);
     };
 
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser)
+            setLoading(false);
         })
         return () => {
             unSubscribe();
         }
     }, [])
 
-    const authInfo = { user, createUser, signIn, logOut, googleSignIn, facebookSignIn, githubSignIn };
+    const authInfo = { user, createUser, signIn, logOut, googleSignIn, facebookSignIn, githubSignIn, resetPassword, loading };
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/PriveatRoute/PrivateRoute.jsx
+++ src/PriveatRoute/PrivateRoute.jsx
@@ -5,12 +5,15 @@ import { Navigate, useLocation } from "react-router-dom";
 
 
 const PrivateRoute = ({ children }) => {
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
+    if (loading) {
+        return <div className="w-10 h-10 my-5 mx-auto animate-[spin_2s_linear_infinite] rounded-full border-8 border-dotted border-sky-600"></div>;
+    }
     if (user) {
         return children
     }
-    return <Navigate to={"/login"}  state={location}></Navigate>
+    return <Navigate to={"/login"} state={location}></Navigate>
 };
 
 export default PrivateRoute;
